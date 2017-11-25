3f110ff9e9698976cfbfb38b2a94a408efe65c37
Jiten Agarwal <jiten@springboard.com>
2017-11-25 19:37:19 +0530

    updated the index.js

--- src/index.js
+++ src/index.js
@@ -15,22 +15,22 @@ const handlers = {
         this.emit('SayHello')
     },
     'SayHello': function () {
-        this.response.speak('Hello World!');
+        this.response.speak('Hello World New !');
         this.emit(':responseReady');
     },
     'AMAZON.HelpIntent': function () {
-        const speechOutput = 'This is the Hello World Sample Skill. ';
-        const reprompt = 'Say hello, to hear me speak.';
+        const speechOutput = 'This is the Hello World Sample Skill. New ';
+        const reprompt = 'Say hello, to hear me speak.New ';
 
         this.response.speak(speechOutput).listen(reprompt);
         this.emit(':responseReady');
     },
     'AMAZON.CancelIntent': function () {
-        this.response.speak('Goodbye!');
+        this.response.speak('Goodbye! New ');
         this.emit(':responseReady');
     },
     'AMAZON.StopIntent': function () {
-        this.response.speak('See you later!');
+        this.response.speak('See you later! New');
         this.emit(':responseReady');
     }
 };
